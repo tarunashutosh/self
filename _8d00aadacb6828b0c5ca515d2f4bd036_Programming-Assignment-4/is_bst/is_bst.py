f4420c4218d5b80ff27a98df4171464ec0a61c6b
tarunashutosh <tarunashutosh.19@gmail.com>
2019-12-18 09:04:12 +0800

    data struct

--- _8d00aadacb6828b0c5ca515d2f4bd036_Programming-Assignment-4/is_bst/is_bst.py
+++ _8d00aadacb6828b0c5ca515d2f4bd036_Programming-Assignment-4/is_bst/is_bst.py
@@ -1,31 +1,33 @@
 #!/usr/bin/python3
-
 import sys, threading
 
 sys.setrecursionlimit(10**7) # max depth of recursion
-threading.stack_size(2**25)  # new thread will get stack of such size
+threading.stack_size(2**27)  # new thread will get stack of such size
 
 def IsBinarySearchTree(tree):
   # Implement correct algorithm here
-  mini = -3**31
-  maxi = 3**31
+  mini = -2**31
+  maxi = 2**31
+  if tree==[]:
+    return True
   def isBST(i): 
     return (isBSTUtil(i, mini, maxi)) 
   
   def isBSTUtil(i, mini, maxi): 
       
-    if len(tree[i])<=1: 
-      print ('1')
-      return True
-  
-    if tree[i][0] < mini or tree[i][0] > maxi: 
-      print ('2')
-      return False
-  
-    return (isBSTUtil(tree[i][1], mini, tree[i][0]-1) and
-      isBSTUtil(tree[i][2], tree[i][0]+1, maxi)) 
+    if i!=-1:
+      if tree[i][0] < mini or tree[i][0] > maxi: 
+        return False
+      ans1 = isBSTUtil(tree[i][1], mini, tree[i][0]-1)
+      ans2 = isBSTUtil(tree[i][2], tree[i][0]+1, maxi)
+      if ans1==None:
+        ans1 = True
+      if ans2==None:
+        ans = True
+      return ans1 and ans2
   ans = isBST(0)
-  print (ans)
+  if ans==None:
+    ans=True
   return ans
 
 def main():
--- _8d00aadacb6828b0c5ca515d2f4bd036_Programming-Assignment-4/is_bst_hard/is_bst_hard.py
+++ _8d00aadacb6828b0c5ca515d2f4bd036_Programming-Assignment-4/is_bst_hard/is_bst_hard.py
@@ -1,14 +1,34 @@
 #!/usr/bin/python3
-
 import sys, threading
 
 sys.setrecursionlimit(10**7) # max depth of recursion
-threading.stack_size(2**25)  # new thread will get stack of such size
+threading.stack_size(2**27)  # new thread will get stack of such size
 
 def IsBinarySearchTree(tree):
   # Implement correct algorithm here
-  return True
-
+  mini = -2**31
+  maxi = 2**31-1
+  if tree==[]:
+    return True
+  def isBST(i): 
+    return (isBSTUtil(i, mini, maxi)) 
+  
+  def isBSTUtil(i, mini, maxi): 
+      
+    if i!=-1:
+      if tree[i][0] < mini or tree[i][0] > maxi: 
+        return False
+      ans1 = isBSTUtil(tree[i][1], mini, tree[i][0]-1)
+      ans2 = isBSTUtil(tree[i][2], tree[i][0], maxi)
+      if ans1==None:
+        ans1 = True
+      if ans2==None:
+        ans = True
+      return ans1 and ans2
+  ans = isBST(0)
+  if ans==None:
+    ans=True
+  return ans
 
 def main():
   nodes = int(sys.stdin.readline().strip())
